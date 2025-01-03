df3c070cd3b4d72d4c797ebac66278268e611528
Josh <joshfriedland37@gmail.com>
2025-01-03 11:53:45 -0500

    Changed paths to contact assets

--- portfolio/src/Contact/Contact.tsx
+++ portfolio/src/Contact/Contact.tsx
@@ -7,15 +7,15 @@ export const Contact = () => {
         </div>
         <ul className='w-1/3 flex flex-col space-y-4'>
             <li className='contact_url'>
-                <img src="/src/assets/contact/emailIcon.png" alt="Email icon" />
+                <img src="/assets/contact/emailIcon.png" alt="Email icon" />
                 <a href="mailto:joshuafriedland7@gmail.com">joshuafriedland7@gmail.com</a>
             </li>
             <li className='contact_url'>
-                <img src="/src/assets/contact/linkedinIcon.png" alt="Linkedin icon" />
+                <img src="/assets/contact/linkedinIcon.png" alt="Linkedin icon" />
                 <a href="https://www.linkedin.com/in/jefriedl/">linkedin.com/in/jefriedl/</a>
             </li>
             <li className='contact_url'>
-                <img src="/src/assets/contact/githubIcon.png" alt="Github icon" />
+                <img src="/assets/contact/githubIcon.png" alt="Github icon" />
                 <a href="https://github.com/joshtopher">github.com/joshtopher</a>
             </li>
         </ul>
