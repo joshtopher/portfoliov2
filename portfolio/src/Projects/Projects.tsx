a3dc3647be86a4a3c5d5bf826556dd9cf534cfe8
Josh <joshfriedland37@gmail.com>
2025-01-04 14:09:13 -0500

    Fixed project section size issue

--- portfolio/src/Projects/Projects.tsx
+++ portfolio/src/Projects/Projects.tsx
@@ -6,7 +6,7 @@ export const Projects = () => {
     <section className='section' id='projects'>
         <h2 className='section_title'>PROJECTS</h2>
         <div>
-          <ul className='w-full h-120 flex justify-center flex-wrap gap-20 p-0 m-0'>            {
+          <ul className='w-full h-120 flex justify-center flex-wrap gap-20 p-0 m-0 overflow-auto'>            {
               projects.map((project, id) => {
                 return <Project key={id} project={project}/>
               })
